8fc48101925749e9345b593010f8d44d2f78964e
Ashutosh Pandey <akpandey1928@gmail.com>
2024-06-04 16:29:21 +0530

    Add Login/Signup UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+// import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
+ReactDOM.render(
+	<ChakraProvider>
 		<BrowserRouter>
-			<ChakraProvider>
-				<App />
-			</ChakraProvider>
+			{/* <ChatProvider> */}
+			<App />
+			{/* </ChatProvider> */}
 		</BrowserRouter>
-	</React.StrictMode>
+	</ChakraProvider>,
+	document.getElementById("root")
 );
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,67 @@
+import {
+	Box,
+	Container,
+	Tab,
+	TabList,
+	TabPanel,
+	TabPanels,
+	Tabs,
+	Text,
+} from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+import { Login } from "../components/Authentication/Login";
+import { Signup } from "../components/Authentication/Signup";
+
 export const HomePage = () => {
+	const history = useHistory();
+
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem("userInfo"));
+
+		if (user) history.push("/chats");
+	}, [history]);
+
 	return (
-		<>
-			<h1>Homepage</h1>
-		</>
+		<Container maxW="xl" centerContent>
+			<Box
+				display="flex"
+				justifyContent="center"
+				p={3}
+				bg="white"
+				w="100%"
+				m="40px 0 15px 0"
+				borderRadius="lg"
+				borderWidth="3px"
+				borderColor="black"
+			>
+				<Text fontSize="4xl" fontFamily="Work sans" fontWeight="bold">
+					GupShup
+				</Text>
+			</Box>
+			<Box
+				bg="white"
+				w="100%"
+				p={4}
+				borderRadius="lg"
+				borderWidth="3px"
+				borderColor="black"
+			>
+				<Tabs isFitted variant="soft-rounded">
+					<TabList mb="1em" gap=".7em">
+						<Tab border="2px">Login</Tab>
+						<Tab border="2px">Sign Up</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>
+							<Login />
+						</TabPanel>
+						<TabPanel>
+							<Signup />
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</Box>
+		</Container>
 	);
 };
