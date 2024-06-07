588e2534f5dc8815ece95eccc29ea8d153f9a6e5
Ashutosh Pandey <akpandey1928@gmail.com>
2024-06-07 23:58:25 +0530

    Add Login/Singup, JWT in backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const chats = require("./data/chats");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -9,21 +11,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	console.log("API is running");
 	res.send("welcome to homepage");
 });
 
-app.get("/api/chat", (req, res) => {
-	res.send(chats);
-});
+app.use("/api/user/", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-	const { id } = req.params;
-	const chat = chats.find((chat) => chat._id === id);
-
-	res.send(chat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(`Server is listening on ${port}`);
