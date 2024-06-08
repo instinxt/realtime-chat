f3da823fa067ea11db4092f5e9bee99445df81b2
Ashutosh Pandey <akpandey1928@gmail.com>
2024-06-08 09:53:21 +0530

    Add 1:1/Group chat endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const chats = require("./data/chats");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user/", userRoutes);
+app.use("/api/chat/", chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
