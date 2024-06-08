025564818f0b95506454afc8662c57f6eea22715
Ashutosh Pandey <akpandey1928@gmail.com>
2024-06-08 23:32:26 +0530

    Complete socketIO backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -64,6 +64,7 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access          Public
 const authUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body;
+	console.log("Inside login auth");
 
 	const user = await User.findOne({ email });
 
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,65 @@
 const express = require("express");
-const chats = require("./data/chats");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-
 dotenv.config();
-connectDB();
-const app = express();
 
-const port = process.env.PORT || 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	console.log("API is running");
-	res.send("welcome to homepage");
-});
-
 app.use("/api/user/", userRoutes);
 app.use("/api/chat/", chatRoutes);
+app.use("/api/message/", messageRoutes);
 
+// Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => {
-	console.log(`Server is listening on ${port}`);
+const server = app.listen(PORT, () => {
+	console.log(`Server running at ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+	pingTimeout: 60000,
+	cors: {
+		origin: "http://localhost:5173",
+	},
+});
+
+io.on("connection", (socket) => {
+	console.log("Connected to socket.io");
+	socket.on("setup", (userData) => {
+		socket.join(userData._id);
+		socket.emit("connected");
+	});
+
+	socket.on("join chat", (room) => {
+		socket.join(room);
+		console.log("User Joined Room: " + room);
+	});
+	socket.on("typing", (room) => socket.in(room).emit("typing"));
+	socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+	socket.on("new message", (newMessageRecieved) => {
+		var chat = newMessageRecieved.chat;
+
+		if (!chat.users) return console.log("chat.users not defined");
+
+		chat.users.forEach((user) => {
+			if (user._id == newMessageRecieved.sender._id) return;
+
+			socket.in(user._id).emit("message recieved", newMessageRecieved);
+		});
+	});
+
+	socket.off("setup", () => {
+		console.log("USER DISCONNECTED");
+		socket.leave(userData._id);
+	});
 });
